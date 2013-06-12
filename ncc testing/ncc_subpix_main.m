745bc40b3d81fe99935ff639c885e17e49363b24
Dylan Koenig <koenigup@gmail.com>
2013-06-12 15:40:59 -0400

    added separate files for subpixel matching, looking forward to dealing with merge conflicts in the future

---
+++ ncc testing/ncc_subpix_main.m
@@ -0,0 +1,84 @@
+%given im1, im2, and feature points (sx1,sy1) in im1 compute ncc across a window in im2 to find
+%correspondence points in im2
+
+numImages = 40;
+numPoints = 500;
+
+%import images from directory direc
+direc = 'C:\Users\render\Desktop\dylan\helicopter images 2\left*.tiff';
+D = dir(direc);
+%sizes for helicopter images 1
+% imHeight = 1936;
+% imWidth = 1456;
+%sizes for helicopter images 2
+imHeight = 1827;
+imWidth = 1306;
+leftImages = uint8(zeros(imHeight,imWidth,numImages));
+for i=1:numImages;
+    leftImages(:,:,i) = imread(['C:\Users\render\Desktop\dylan\helicopter images 2\' D(i).name]);
+end
+display('finished loading images');
+%%
+
+% im1rows = zeros(numPoints,1,numImages);
+% im1cols = im1rows;
+% im2rows = im1rows;
+% im2cols = im1rows;
+
+%contains all points for all images, including newly acquired ones if
+%points went out of frame
+%in the form: [imNcols,imNrows]
+points = zeros(numPoints,2,numImages);
+%contains only points with correspondences (ie no newly acquired points)
+%in the form: [imNcols,imNrows,imN+1cols,imN+1rows]
+correspondences = zeros(numPoints,4,numImages-1);
+
+%resize scale factor
+imScale = 3;
+
+%unroll first image
+display('setting up first image');
+currIm2 = leftImages(:,:,1);
+resizedIm2 = imresize(currIm2,imScale);
+[x1, y1, v1] = harris(currIm2);
+[points(:,1,1), points(:,2,1), ~] = suppress(x1,y1,v1);
+%instantiate im2pts
+display('beginning image processing');
+for i=1:(numImages-1)
+    display(['processing images ' num2str(i) ' and ' num2str(i+1)]);
+    currIm1 = currIm2;
+    resizedIm1 = resizedIm2;
+    currIm2 = leftImages(:,:,i+1);
+    resizedIm2 = imresize(currIm2,imScale);
+    correspondences(:,2,i) = points(:,2,i);
+    correspondences(:,1,i) = points(:,1,i);
+    [points(:,2,i+1),points(:,1,i+1),correspondences(:,4,i),correspondences(:,3,i)] = ncc_match(currIm1,currIm2,resizedIm1,resizedIm2,points(:,2,i),points(:,1,i));
+end
+display('finished processing images');
+%%
+%save dual image figs to show correspondences
+
+%%
+
+%save figs to make video
+% D = dir('C:/Users/render/Desktop/dylan/helicopter images/left*.png');
+fig = figure;
+for i=1:numImages
+    set(fig,'PaperPositionMode','auto');
+    imshow(leftImages(:,:,i));
+    hold on;
+%     plot(points(:,1,i),points(:,2,i),'r.'); %plot all points
+    %plot correspondences
+    if (i==1)
+        plot(correspondences(:,1,i),correspondences(:,2,i),'r.');
+    else
+        plot(correspondences(:,3,i-1),correspondences(:,4,i-1),'r.');
+    end
+    hold off;
+%     saveas(fig,['C:/Users/render/Desktop/dylan/ncc testing/results 6-5-13/' int2str(i)],'png');
+    print(fig,'-dpng', '-r0', ['C:/Users/render/Desktop/dylan/ncc testing/results/' int2str(i) '.png']);
+end
+display('finished saving figures');
+%%
+makeVideo('C:/Users/render/Desktop/dylan/ncc testing/results',numImages);
+display('finished making video');

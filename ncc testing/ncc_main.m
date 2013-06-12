9c400a9eb7fd76c92b86f8d6329f70243ced3596
Dylan Koenig <koenigup@gmail.com>
2013-06-12 10:34:28 -0400

    moved some code around

--- ncc testing/ncc_main.m
+++ ncc testing/ncc_main.m
@@ -38,7 +38,8 @@ imScale = 5;
 
 %unroll first image
 display('setting up first image');
-currIm2 = imresize(leftImages(:,:,1),imScale);
+currIm2 = leftImages(:,:,1);
+resizedIm2 = imresize(currIm2,imScale);
 [x1, y1, v1] = harris(currIm2);
 [points(:,1,1), points(:,2,1), ~] = suppress(x1,y1,v1);
 %instantiate im2pts
@@ -46,10 +47,12 @@ display('beginning image processing');
 for i=1:(numImages-1)
     display(['processing images ' num2str(i) ' and ' num2str(i+1)]);
     currIm1 = currIm2;
-    currIm2 = imresize(leftImages(:,:,i+1),imScale);
+    resizedIm1 = resizedIm2;
+    currIm2 = leftImages(:,:,i+1);
+    resizedIm2 = imresize(currIm2,imScale);
     correspondences(:,2,i) = points(:,2,i);
     correspondences(:,1,i) = points(:,1,i);
-    [points(:,2,i+1),points(:,1,i+1),correspondences(:,4,i),correspondences(:,3,i)] = ncc_match(currIm1,currIm2,points(:,2,i),points(:,1,i));
+    [points(:,2,i+1),points(:,1,i+1),correspondences(:,4,i),correspondences(:,3,i)] = ncc_match(resizedIm1,resizedIm2,points(:,2,i),points(:,1,i));
 end
 display('finished processing images');
 %%
